884fabd4a87a60c12a6faa24ee68558894118b16
Joao Dionisio <jd1583@hotmail.com>
2017-11-25 15:12:21 -0300

    Segundo commit Aula04ListView_02

--- app/src/main/java/com/example/goddionisio/aula04listview_02/MainActivity.java
+++ app/src/main/java/com/example/goddionisio/aula04listview_02/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.goddionisio.aula04listview_02;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity{
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.output);
+
+        String[] diasSemana = {"Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, diasSemana);
+        setListAdapter(adapter);
+
     }
 }
